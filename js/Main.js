029af1521915a736561d90f3a6107e42104bda09
NicoGuido10 <nicolasguido10@gmail.com>
2022-09-07 18:57:02 -0300

    Continuo con el carrito de compras

--- js/Main.js
+++ js/Main.js
@@ -1,60 +1,115 @@
 //Variables 
 let AllContainer = document.querySelector(".MainClothes");
 let containetcart = document.querySelector(".card-items");
-let buythisCart =[];
+let TotalPrice = document.querySelector(".PriceTotal");
 
+
+
+
+let buythisCart = [];
+let TotalProduct = 0;
 //Functions
 LoadProductEvent();
-function LoadProductEvent(){
+function LoadProductEvent() {
     AllContainer.addEventListener("click", AddProduct);
+
+    containetcart.addEventListener("click", deleteProduct);
 }
 
-function AddProduct(i){
+function AddProduct(i) {
     i.preventDefault();
-    if (i.target.classList.contains("button__index")){
+    if (i.target.classList.contains("button__index")) {
         const ProductSelec = i.target.parentElement;
         readTheContent(ProductSelec);
-        
+
     }
 
 }
 
-function readTheContent(product){
+function deleteProduct(i) {
+    if (i.target.classList.contains("Product-fin")) {
+        const deleteId = i.target.getAttribute("data-id");
+
+       buythisCart.forEach(value =>{
+        if (value.id == deleteId){
+        let priceReduce = parseFloat(value.price) * parseFloat(value.amount);
+        TotalProduct = TotalProduct - priceReduce;
+        TotalProduct = TotalProduct.toFixed(2);
+
+
+        }
+       });
+        buythisCart = buythisCart.filter(product => product.id !== deleteId)
+
+    }
+    loadHtml();
+
+}
+
+function readTheContent(product) {
     const infoProduct = {
         image: product.querySelector('img').src,
         title: product.querySelector("p").textContent,
         price: product.querySelector("p span").textContent,
         id: product.querySelector("a").getAttribute("data-id"),
         amount: 1
-       
+
     }
 
-    buythisCart =[...buythisCart, infoProduct]
-    loadHtml();
 
-    console.log(infoProduct);
+    TotalProduct = parseFloat(TotalProduct) + parseFloat(infoProduct.price);
+    TotalProduct = TotalProduct.toFixed(2);
+
+
+
+
+    const ExSid = buythisCart.some(product => product.id === infoProduct.id);
+    if (ExSid) {
+        const ExSid = buythisCart.map(product => {
+            if (product.id === infoProduct.id) {
+                product.amount++;
+                return product;
+            }
+            else {
+                return product
+            }
+        });
+        buythisCart = [...ExSid];
+    }
+    else {
+        buythisCart = [...buythisCart, infoProduct]
+    }
+    loadHtml();
 
 }
 
-function loadHtml(){
-    buythisCart.forEach( product =>{
-        const {image,title,price,amount,id} = product;
+function loadHtml() {
+    clearHtml();
+    buythisCart.forEach(product => {
+        const { image, title, price, amount, id } = product;
         const row = document.createElement("div");
         row.classList.add("camiseta");
         row.innerHTML = `
         <div class="item">
-                        <img src="./img/camiseta1.png" alt="">
+                        <img src="${image}" alt="">
                         <div class="item-content">
-                            <h5>name of product name of product name of product</h5>
-                            <h5 class="cart-price">45.50$</h5>
-                            <h6>Amount: 3</h6>
+                            <h5>${title}</h5>
+                            <h5 class="${price}">$</h5>
+                            <h6> Amount: ${amount}</h6>
                         </div>
-                        <span>X</span>
+                        <span class="Product-fin" data-id="${id}">X</span>
                     </div>
         
         `;
-
+        /*<span class="Product-fin" data-id="${id}">X</span>*/
+        /*<button class="Product-fin" data-id="${id}">X</button>*/
         containetcart.appendChild(row);
+
+        TotalPrice.innerHTML = TotalProduct;
+
     });
 
+}
+function clearHtml() {
+    containetcart.innerHTML = "";
 }

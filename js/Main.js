b56c448b18e06709315a9cbf94e9e50bacab914f
NicoGuido10 <nicolasguido10@gmail.com>
2022-09-09 23:53:06 -0300

    Sigo modificando sass + javascript

--- js/Main.js
+++ js/Main.js
@@ -2,12 +2,13 @@
 let AllContainer = document.querySelector(".MainClothes");
 let containetcart = document.querySelector(".card-items");
 let TotalPrice = document.querySelector(".PriceTotal");
-
-
-
-
+let contadorAmount = document.querySelector(".ContadorProd");
 let buythisCart = [];
 let TotalProduct = 0;
+let ProductCount = 0;
+
+
+
 //Functions
 LoadProductEvent();
 function LoadProductEvent() {
@@ -30,37 +31,42 @@ function deleteProduct(i) {
     if (i.target.classList.contains("Product-fin")) {
         const deleteId = i.target.getAttribute("data-id");
 
-       buythisCart.forEach(value =>{
-        if (value.id == deleteId){
-        let priceReduce = parseFloat(value.price) * parseFloat(value.amount);
-        TotalProduct = TotalProduct - priceReduce;
-        TotalProduct = TotalProduct.toFixed(2);
+        buythisCart.forEach(value => {
+            if (value.id == deleteId) {
+                let priceReduce = parseFloat(value.price) * parseFloat(value.amount);
+                TotalProduct = TotalProduct - priceReduce;
+                TotalProduct = TotalProduct.toFixed(2);
 
 
-        }
-       });
+            }
+        });
         buythisCart = buythisCart.filter(product => product.id !== deleteId)
-
+        ProductCount --;
     }
+    if(buythisCart.length ===0);
+    TotalPrice.innerHTML = 0;
+    contadorAmount.innerHTML = 0; 
+
     loadHtml();
 
 }
-
+/**Tomo  recursos desde el html */
 function readTheContent(product) {
     const infoProduct = {
         image: product.querySelector('img').src,
-        title: product.querySelector("p").textContent,
-        price: product.querySelector("p span").textContent,
+        title: product.querySelector("h4").textContent,
+        price: product.querySelector("p , span").textContent,
         id: product.querySelector("a").getAttribute("data-id"),
         amount: 1
 
     }
 
+    
 
     TotalProduct = parseFloat(TotalProduct) + parseFloat(infoProduct.price);
     TotalProduct = TotalProduct.toFixed(2);
 
-
+/**Tomo  recursos desde el html */
 
 
     const CountExSid = buythisCart.some(product => product.id === infoProduct.id);
@@ -78,11 +84,14 @@ function readTheContent(product) {
     }
     else {
         buythisCart = [...buythisCart, infoProduct]
-    }
+        ProductCount ++;
+    }    
     loadHtml();
 
 }
 
+
+/**PRINT */
 function loadHtml() {
     clearHtml();
     buythisCart.forEach(product => {
@@ -93,23 +102,25 @@ function loadHtml() {
         <div class="item">
                         <img src="${image}" alt="">
                         <div class="itemContainer">
-                            <h5>${title}</h5>
-                            <h5 class="${price}">$</h5>
+                            <h4>${title}</h4>
+                            <h5 class="priceCart">${price}</h5>
                             <h6> Amount: ${amount}</h6>
-                        </div>
+                        </div class="buttonDeleteCart" >
                         <span class="Product-fin" data-id="${id}">X</span>
                     </div>
         
         `;
-        
-        
+
+
         containetcart.appendChild(row);
 
         TotalPrice.innerHTML = TotalProduct;
 
+        contadorAmount.innerHTML = ProductCount;
     });
 
 }
+/**PRINT */
 function clearHtml() {
     containetcart.innerHTML = "";
 }

76ecc8c55e5749ca997a7627fce071160ac20ccc
NicoGuido10 <nicolasguido10@gmail.com>
2022-10-04 00:19:52 -0300

    Modifico el Carrito de compra

--- js/Cart.js
+++ js/Cart.js
@@ -10,7 +10,7 @@ let CarritoProductos = []
 
 ClickAgregar.forEach(btn => {
     btn.addEventListener("click", AddCarritoProduct) //Capturo el boton
-   
+
 })
 
 function AddCarritoProduct(e) {
@@ -34,37 +34,37 @@ function AddCarritoProduct(e) {
 function AdditemCart(NewItemCart) { //Funcion que agrega productos al carrito
     const ElementCount = ContainerCart.getElementsByClassName("ElementCount")
 
-    
-//Alerta emergente dentro de la funcion de agregar producto
+
+    //Alerta emergente dentro de la funcion de agregar producto
     Toastify({
         text: "Producto Agregado",
-        duration: 1500,  
+        duration: 1500,
         newWindow: true,
         close: true,
-        gravity: "top", 
-        position: "right", 
-        stopOnFocus: true, 
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
         style: {
-          background: "#1B2B3B",
+            background: "#1B2B3B",
         },
-        onClick: function(){} 
-      }).showToast();
+        onClick: function () { }
+    }).showToast();
 
-   //Alerta emergente dentro de la funcion de agregar producto
+    //Alerta emergente dentro de la funcion de agregar producto
 
 
 
     for (let i = 0; i < CarritoProductos.length; i++) { //Le doy funcion de suma al carrito y que no repita productos
         if (CarritoProductos[i].title.trim() === NewItemCart.title.trim()) {
             CarritoProductos[i].cantidad++;
-            
+
             const InputValue = ElementCount[i]
             InputValue.value++;
             CartTotal();
             return null;
-            
+
         }
-       
+
     }
 
 
@@ -84,11 +84,14 @@ function CartLook() { //Cada vez que presiono click guardo en la variable todos 
         trElement.classList.add("ItemCart")
         const content = `
         <th scope="row">1</th>
-        <td class="cartProducts"><img class="imgProductCart" src="${item.img}" alt=""></td>
-        <h6 class="title">${item.title}</h6>
-        <td class="CartPrecio">${item.precio}</td>
+        <td class="cartProducts"><img class="imgProductCart" src="${item.img}" alt="">  <h6 class="title">${item.title}</h6></td>
+        <td class="CartPrecio"> ${item.precio}</td>
         <input type="number" min="1" value=${item.cantidad} class="ElementCount">
-        <button class="deleteCart">x</button>
+        <button class="deleteCart"><svg xmlns="http://www.w3.org/2000/svg"
+         width="24" height="24" viewBox="0 0 24 24">
+         <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+         </svg></span></button>
+        
         </td>
         `
         trElement.innerHTML = content

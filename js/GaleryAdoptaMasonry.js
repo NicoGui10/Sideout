525e99c270161ef6e189efda502fe4a2f39a6037
NicoGuido10 <nicolasguido10@gmail.com>
2022-10-06 00:28:51 -0300

    Sigo trabajando en el responsive

--- js/GaleryAdoptaMasonry.js
+++ js/GaleryAdoptaMasonry.js
@@ -3,8 +3,8 @@ let elem = document.querySelector('.GaleryContainer');
 let msnry = new Masonry( elem, {
   // options
   itemSelector: '.GaleryItem',
-  columnWidth: 240,
-  gutter:22,
-  isfitwhidth: true,
+  columnWidth: 230,
+        gutter: 20,
+        isFitWidth: true
 });
 
